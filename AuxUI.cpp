f05b0a8738650a8d4cfc016231f6dd1b109d2f3f
Francisco <kikinhobiaso@gmail.com>
2020-05-29 11:52:43 -0300

    AuxUI.cpp cleaning and documenting

--- AuxUI.cpp
+++ AuxUI.cpp
@@ -22,18 +22,20 @@ ui::AuxUI::AuxUI()
     g_signal_connect(gtkEntryThingSearch, "focus-out-event", G_CALLBACK(cb_onFocusOutEvent), NULL);
 }
 
-
+/* [callback] on hit enter key */
 void ui::AuxUI::cb_onActive(GtkWidget* widget, gpointer data)
 {
     ui::AuxUI::cb_doSearchThing(NULL, NULL);
 }
 
+/* [callback] on grab focus mouse */
 void ui::AuxUI::cb_onFocusInEvent(GtkWidget* widget, gpointer data)
 {
     GtkEntryBuffer* gtkEntryBuffer = gtk_entry_get_buffer(GTK_ENTRY(gtkEntryThingSearch));
     gtk_entry_buffer_delete_text(gtkEntryBuffer, 0, gtk_entry_buffer_get_length(gtkEntryBuffer));    
 }
 
+/* [callback] on leave focus mouse */
 void ui::AuxUI::cb_onFocusOutEvent(GtkWidget* widget, gpointer data)
 {
     GtkEntryBuffer* gtkEntryBuffer = gtk_entry_get_buffer(GTK_ENTRY(gtkEntryThingSearch));
@@ -41,62 +43,55 @@ void ui::AuxUI::cb_onFocusOutEvent(GtkWidget* widget, gpointer data)
     gtk_entry_buffer_set_text(gtkEntryBuffer, buffer.c_str(), buffer.size());
 }
 
+/* [callback] get thing from stuffbook */
 void ui::AuxUI::cb_doSearchThing(GtkWidget* widget, gpointer data)
 {  
+    /* grab data from text input */
     std::string entryText = gtk_entry_get_text(GTK_ENTRY(gtkEntryThingSearch));
+    /* if data was provied as 2º argument, use it */
     if (data != NULL)
         entryText = (char *)data;
     
-    gint row_count = 0;
-    gboolean valid;
-    GtkTreeIter iter;
-    GtkTreeModel* model = gtk_tree_view_get_model(gStuffBook->getTreeViewStuffBook());
+    gint row_count = 0; // count rows, not used  //
+    gboolean valid; // check error //
+    GtkTreeIter iter; // our node to iterate through the model //
+    GtkTreeModel* model = gtk_tree_view_get_model(gStuffBook->getTreeViewStuffBook()); // grab model //
 
     /* Get the first iter in the list */
     valid = gtk_tree_model_get_iter_first(model, &iter);
 
-    bool founded = false;
-    while (valid && !founded)
+    bool founded = false; // start as not founded //
+    while (valid && !founded) // while iterating and not founded //
     {
         GtkTreeIter iterChildren;
         gboolean validChildren;
 
-
         validChildren = gtk_tree_model_iter_children(model, &iterChildren, &iter);
-        while (validChildren && !founded)
+        while (validChildren && !founded) // iterate through children //
         {
             gchar* str_data;
-            gtk_tree_model_get(model, &iterChildren, 0, &str_data, -1);
+            gtk_tree_model_get(model, &iterChildren, 0, &str_data, -1); // grab data from model //
             
-            if (std::string(str_data) == entryText)
+            if (std::string(str_data) == entryText) // compare //
             {                                
-                GtkTreePath* path = gtk_tree_model_get_path(model, &iter);
-                //gtk_tree_selection_select_path(gtk_tree_view_get_selection(GTK_TREE_VIEW(gStuffBook->gtkTreeViewStuffBook)), path);
-                gtk_tree_view_expand_row(gStuffBook->getTreeViewStuffBook(), path, false);
-
-
+                GtkTreePath* path = gtk_tree_model_get_path(model, &iter);                
+                gtk_tree_view_expand_row(gStuffBook->getTreeViewStuffBook(), path, false); // expand row //
                 GtkTreePath* pathCell = gtk_tree_model_get_path(model, &iterChildren);
-
-                gtk_tree_selection_select_iter(gtk_tree_view_get_selection(gStuffBook->getTreeViewStuffBook()), &iterChildren);
-
-                gtk_tree_view_scroll_to_cell(gStuffBook->getTreeViewStuffBook(), pathCell, NULL, false, 1.0, 1.0);
-                
+                gtk_tree_selection_select_iter(gtk_tree_view_get_selection(gStuffBook->getTreeViewStuffBook()), &iterChildren); // select item //
+                gtk_tree_view_scroll_to_cell(gStuffBook->getTreeViewStuffBook(), pathCell, NULL, false, 1.0, 1.0); // adjust scroll window //                
                 gtk_tree_path_free(path);                
-
-                // gtk_widget_show(GTK_WIDGET(gStuffBook->gtkTreeViewStuffBook));
-                //gtk_tree_view_set_focus
-
-                founded = true;
+                founded = true; // we founded //
             }
-            g_free(str_data);
-            validChildren = gtk_tree_model_iter_next(model, &iterChildren);
+            g_free(str_data); // clean //
+
+            validChildren = gtk_tree_model_iter_next(model, &iterChildren); // jump to next data //
         }
         
-
-        valid = gtk_tree_model_iter_next(model, &iter);
-        row_count++;
+        valid = gtk_tree_model_iter_next(model, &iter); // jump to next data //
+        row_count++; // increment row //
     }
 
+    // print msg //
     if (!founded)
     {
         std::string statusMsg = "Thing [" + entryText + "] not founded!";
