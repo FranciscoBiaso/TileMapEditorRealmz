c3169177955642af213b653bb2f6484f80b2de5d
Francisco <kikinhobiaso@gmail.com>
2020-05-29 18:29:00 -0300

    Comment - aligning details

--- AuxUI.h
+++ AuxUI.h
@@ -18,6 +18,7 @@ namespace ui {
 		static GObject* gtkStatusBar;   
 
 	public:
+		
 		/**  
 		 *	constructor
 		 */
@@ -27,35 +28,35 @@ namespace ui {
 		 *  @brief This method get GtkStatusbar.
 		 */
 		static GtkStatusbar* getStatusBar() { return GTK_STATUSBAR(gtkStatusBar); }
-
-	   /**
-		*  @brief This method searches for a thing by name. 
-		*  If find then select it into user interface GtkTreeView.
-		*  @param name name to be searched.		
-		*  @return true if found and false otherwise.
-		*/
+		
+	    /**
+		 *  @brief This method searches for a thing by name. 
+		 *  If find then select it into user interface GtkTreeView.
+		 *  @param name name to be searched.		
+		 *  @return true if found and false otherwise.
+		 */
 		static bool searchThingByName(std::string name);
 
-	   /**
-		*  @brief This method cleans text input when focused.
-		*  @param widget that will recieve the signal.		
-		*  @param data extra information if needed. 		
-		*/
+	    /**
+		 *  @brief This method cleans text input when focused.
+		 *  @param widget that will recieve the signal.		
+		 *  @param data extra information if needed. 		
+		 */
 		static void cb_onFocusInEvent(GtkWidget* widget, gpointer data);
 
-	   /**
-		*  @brief This method add text input "..." when not focused.
-		*  @param widget that will recieve the signal.	
-		*  @param data extra information if needed.
-		*/
+	    /**
+		 *  @brief This method add text input "..." when not focused.
+		 *  @param widget that will recieve the signal.	
+		 *  @param data extra information if needed.
+		 */
 		static void cb_onFocusOutEvent(GtkWidget* widget, gpointer data);		
 
-	   /**
-		*  @brief This method call searchThingByName when activated.
-		*  @param widget that will recieve the signal.	
-		*  @param data extra information if needed.
-		*  @see searchThingByName
-		*/		
+	    /**
+		 *  @brief This method call searchThingByName when activated.
+		 *  @param widget that will recieve the signal.	
+		 *  @param data extra information if needed.
+		 *  @see searchThingByName
+		 */		
 		static void cb_onActive(GtkWidget* widget, gpointer data);
 	};
 
