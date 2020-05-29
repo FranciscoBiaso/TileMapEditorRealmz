96f2bf42d417267ea7c50d3adbce25f1b1ce37af
Francisco <kikinhobiaso@gmail.com>
2020-05-29 18:32:33 -0300

    Comment - aligning details, class AuxUI

--- AuxUI.h
+++ AuxUI.h
@@ -2,7 +2,6 @@
 #include <gtk/gtk.h>
 #include "MapResources.h"
 #include "StuffBookUI.h"
-    
 
 namespace ui {
 	/*!
@@ -11,7 +10,7 @@ namespace ui {
 
 		This class represents all auxiliary graphical interface.
 	 */
-	class AuxUI 
+	class AuxUI
 	{
 	private:
 		static GObject* gtkEntryThingSearch;   
@@ -19,44 +18,44 @@ namespace ui {
 
 	public:
 		
-		/**  
+		/**
 		 *	constructor
 		 */
-		AuxUI();  
+		AuxUI();
 
 		/**
 		 *  @brief This method get GtkStatusbar.
 		 */
 		static GtkStatusbar* getStatusBar() { return GTK_STATUSBAR(gtkStatusBar); }
 		
-	    /**
-		 *  @brief This method searches for a thing by name. 
-		 *  If find then select it into user interface GtkTreeView.
-		 *  @param name name to be searched.		
+		/**
+		 *  @brief This method searches for a thing by name.
+		 *  If find then select it into user interface GtkTreeView
+		 *  @param name name to be searched.
 		 *  @return true if found and false otherwise.
 		 */
 		static bool searchThingByName(std::string name);
 
-	    /**
+		/**
 		 *  @brief This method cleans text input when focused.
-		 *  @param widget that will recieve the signal.		
-		 *  @param data extra information if needed. 		
+		 *  @param widget that will recieve the signal.
+		 *  @param data extra information if needed.
 		 */
 		static void cb_onFocusInEvent(GtkWidget* widget, gpointer data);
 
-	    /**
+		/**
 		 *  @brief This method add text input "..." when not focused.
-		 *  @param widget that will recieve the signal.	
+		 *  @param widget that will recieve the signal.
 		 *  @param data extra information if needed.
 		 */
-		static void cb_onFocusOutEvent(GtkWidget* widget, gpointer data);		
+		static void cb_onFocusOutEvent(GtkWidget* widget, gpointer data);
 
-	    /**
+		/**
 		 *  @brief This method call searchThingByName when activated.
-		 *  @param widget that will recieve the signal.	
+		 *  @param widget that will recieve the signal.
 		 *  @param data extra information if needed.
 		 *  @see searchThingByName
-		 */		
+		 */
 		static void cb_onActive(GtkWidget* widget, gpointer data);
 	};
 
