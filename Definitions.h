44e5465efb0c7b0ec600ce7e9f9fe9b4bae9150f
Francisco <kikinhobiaso@gmail.com>
2020-06-02 07:45:13 -0300

    Added definition file
    This file will be used to declare definitions that will be used throughout the project.

--- Definitions.h
+++ Definitions.h
@@ -4,3 +4,13 @@
 *  GRID SIZE 
 */
 #define REALMZ_GRID_SIZE 32
+
+/**
+ * ENUM IMG SIZES.
+ */
+enum class IMG_SIZE {
+    IMG_SIZE_32X32, /**< enum IMG_SIZE_32X32  */
+    IMG_SIZE_32X64, /**< enum IMG_SIZE_32X64  */
+    IMG_SIZE_64X32, /**< enum IMG_SIZE_64X32  */
+    IMG_SIZE_64X64, /**< enum IMG_SIZE_64X64  */
+};
