42a687de3c33c33665faa91417562afd242232ca
Francisco <kikinhobiaso@gmail.com>
2020-06-08 18:59:44 -0300

    Fixed dangerous reference error in class Vec2
    Overload operator () was removed from this version.

--- Vec2.h
+++ Vec2.h
@@ -16,52 +16,60 @@ namespace math {
 	class Vec2
 	{
 	private:
-		T xy[VEC2_SIZE];
+		T x, y;
 	public:
 		/**
 		 * default constructor.
 		 * 
 		 */
-		Vec2() { xy[AT_X] = 0; xy[AT_Y] = 0; }
+		Vec2() : x(0),y(0) {}
 
 		/**
 		 * 2º constructor.
-		 * 
+		 *
 		 * @param x position.
 		 * @param y position.
 		 */
-		Vec2(T x, T y) { xy[AT_X] = x; xy[AT_Y] = y; }
+		Vec2(T x, T y) : x(x), y(y) {}
+
+		/**
+		 * 3º copy constructor.
+		 *
+		 * @param v Vec2 to copy.
+		 */
+		Vec2(const Vec2& v) { x = v.getX(); y = v.getY(); }
+
 
 		/**
 		 * @brief This method gets x position.
 		 * \return x.
 		 */
-		T getX() const { return xy[AT_X]; }
+		T getX() const { return x; }
 
 		/**
 		 * @brief This method gets y position.
 		 * \return y.
 		 */
-		T getY() const { return xy[AT_Y]; }
+		T getY() const { return y; }
 
 		/**
 		 * @brief This method sets x position.
 		 * @param x.
 		 */
-		void setX(T x) { xy[AT_X] = x; }
+		void setX(T x) { this->x  = x; }
 
 		/**
 		 * @brief This method sets y position.
 		 * @param y.
 		 */
-		void setY(T y) { xy[AT_Y] = y; }
+		void setY(T y) { this->y = x; }
 
 		/**
 		 * @brief This method sets xy position.
 		 * @param x.
 		 * @param y.
 		 */
-		void setXY(T x, T y) { xy[AT_X] = x; xy[AT_Y] = y; }
+		void setXY(T x, T y) { this->x = x; this->y = y;}
 
 
 		/**
@@ -71,17 +79,26 @@ namespace math {
 		{
 			if (index < 0 || index > 1)
 				throw def::ReturnMsg::ARRAY_OUT_OF_RANGE;
-			return xy[index];
+			return index ? y : x;
 		}
 
 		/**
 		 * @brief This method sets position of index [0,1].
 		 */
-		T& operator()(int index)
+		/*
+		T & operator()(int index)
 		{
 			if (index < 0 || index > 1)
 				throw def::ReturnMsg::ARRAY_OUT_OF_RANGE;
-			return xy[index];
+			return index ? y : x;
+		}
+		*/
+
+		Vec2& operator=(const Vec2& v2)
+		{
+			this->x = v2.getX();
+			this->y = v2.getY();
+			return *this;
 		}
 
 	};
