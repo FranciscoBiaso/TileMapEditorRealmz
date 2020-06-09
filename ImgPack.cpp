58931091f2025265ecbf81f93fd1dd3faf6e9f50
Francisco <kikinhobiaso@gmail.com>
2020-06-09 11:10:36 -0300

    Update of class ImgPack [significant]
    The insertion algorithm regressed to linear time (the algorithm was
    logarithm).
    The removal algorithm regressed to polynomial time O(n^2) (the algorithm
    was logarithm (O(log n) + O(1)).
    The regression was performed because it requieres the implementation of
    a particular data strucutre (A tree with together with a doubly linked
    list[dld]). This strcuture was built using std::map plus private dld but
    as it is not possible to store the pointer inside the map class, was
    necessary implement the current data strcuture wich is temporary.

--- ImgPack.cpp
+++ ImgPack.cpp
@@ -1,7 +1,36 @@
 #include "ImgPack.h"
 
 
-void data::ImgPack::addImgObj(std::string name, GdkPixbuf* srcImg, def::IMG_SIZE size)
+data::ImgPack::ImgPack()
 {
-	//map.insert(std::pair(name, ImgObj(size)));
+	textureAtlas = new TextureAtlas(TEXTURE_ATLAS_MAX_WIDTH, TEXTURE_ATLAS_MAX_WIDTH);
+}
+
+void data::ImgPack::addImgObj(std::string name,const GdkPixbuf* srcImg,const def::IMG_SIZE size)
+{	
+	std::vector<math::Vec2<int>> refs = textureAtlas->addAddImgs(srcImg, size); // add img into texture atlas and grab reference //
+	imgVec.push_back(ImgObj(name,size,refs)); // add img to the end of the vector //
+}
+
+void data::ImgPack::delImgObj(std::string name)
+{
+	// try to find ImgObj by name // O(n)
+	std::vector<data::ImgObj>::iterator it = std::find_if(imgVec.begin(), imgVec.end(), [&, name](data::ImgObj & img) {
+		if (img.getName() == name)
+			return true;
+		return false;
+			});
+	// if founded //
+	if (it != imgVec.end())
+	{
+		// remove imgs from texture atlas O(n) //
+		textureAtlas->delImgObj(it, imgVec.end());
+		// erase the image O(1) //
+		imgVec.erase(it);
+	}
+}
+
+data::TextureAtlas* data::ImgPack::getTextureAtlas() const
+{
+	return textureAtlas;
 }
--- ImgPack.h
+++ ImgPack.h
@@ -2,41 +2,58 @@
 
 #include "ImgObj.h"
 #include "TextureAtlas.h"
-#include <map>
 #include <string>
 #include <gdk/gdk.h>
+
+
 namespace data {
+	/*!
+		ImgPack class data structure
+		============================
+
+		This class represents a data structure capable of storing ImgObj objects and also managing memory sharing of real images (32x32, 32x64,64x32,64x64).
+	 */
 	class ImgPack
 	{
 	private:
-		std::map<std::string, data::ImgObj> map;
-		data::TextureAtlas textureAtlas;
-
+		std::vector<data::ImgObj> imgVec;
+		data::TextureAtlas* textureAtlas;
 		/**
 		 * @brief This method is used to increase memory when needed.		 
 		 */
 		void resizeTextureAtlas();
-
 	public:
 		/**
-		 * @brief This method add <name,ImgObj> into std::map and fill textureAtlas with srcImg pixelBuf.
-		 * @param name map key of the ImgObj to be added.
-		 * @param srcImg pixel buffer region with pixels to fill textureAtlas.
-		 * @param size ImgSize to be copied.
+		 * constructor.
 		 */
-		void addImgObj(std::string name, GdkPixbuf* srcImg, def::IMG_SIZE size);
+		ImgPack();
+		/**
+		 * @brief This method add ImgObj into this data structure.
+		 * @param name key of the ImgObj to be added.
+		 * @param srcImg pixel buffer region with pixels to fill textureAtlas.
+		 * @param size ImgSize to be copied (32x32, 32x64, 64x32, 64x64).
+		 */
+		void addImgObj(std::string name, const GdkPixbuf* srcImg, const def::IMG_SIZE size);
 
 		/**
-		 * @brief This method delete <name,ImgObj> from std::map and images from textureAtlas.
-		 * @param name map key of the ImgObj to be deleted.
+		 * @brief This method delete ImgObj from this data strcture.
+		 * @param name key of the ImgObj to be deleted.
 		 */
 		void delImgObj(std::string name);
 
 		/**
-		 * @brief This method gets ImgObj * with complexity O(log(n)). 
-		 * Used when resources will be loaded.
-		 * \return ImgObj *.
+		 *  @brief This method gets data::TextureAtlas*.
 		 */
-		const data::ImgObj* getImgObjPtr();
+		data::TextureAtlas* getTextureAtlas() const;
+
+		/**
+		 *  @brief This method gets vector<data::ImgObj>&.
+		 */
+		std::vector<data::ImgObj>& getMap() { return imgVec; }
+
+		/**
+		 *  @brief This method gets count of ImgObj.
+		 */
+		int getCountImgs() { return imgVec.size(); }
 	};
-};
+}
