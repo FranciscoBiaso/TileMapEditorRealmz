dc5e86404ed46990e20aff00f6df16805b149569
Francisco <kikinhobiaso@gmail.com>
2020-06-09 19:29:22 -0300

    Update DebugTexturAtlas class
    Refactoring code to fits the a new structure model, ImgPack class.

--- DebugTextureAtlas.cpp
+++ DebugTextureAtlas.cpp
@@ -1,36 +1,121 @@
 #include "DebugTextureAtlas.h"
-
+#include "MapResources.h"
+#include "GraphicsTool.h"
+#include "ImgPackUI.h"
+#include <cstdlib> // rand
 
 GtkWidget* DebugTextureAtlas::drawingArea;
 cairo_surface_t* DebugTextureAtlas::surface = nullptr;
 GObject* DebugTextureAtlas::gtkMapFrame;
 GtkWidget* DebugTextureAtlas::p_window = nullptr;
 
+int DebugTextureAtlas::imgNamesInt = 0;
+std::vector<std::string> DebugTextureAtlas::imgNames;
+std::stack<std::string> DebugTextureAtlas::operations;
+
 namespace GtkUserInterface { extern GtkBuilder* builder; }
+
+extern data::MapResources* gResources;
+extern ui::GraphicsTool* gGraphicsTool;
+extern ui::ImgPackUI* gImgPackUI;
 
 DebugTextureAtlas::DebugTextureAtlas(GdkPixbuf * pixlBuff)
 {
     p_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(p_window), "Debug window");
-    gtk_window_set_default_size(GTK_WINDOW(p_window), 500, 300);
+    gtk_window_set_default_size(GTK_WINDOW(p_window), 640, 640);
+    gtk_window_set_position(GTK_WINDOW(p_window), GTK_WIN_POS_CENTER_ON_PARENT);
 
-	static GObject* gtkViewportImgSrc;
 	gtkMapFrame = gtk_builder_get_object(GtkUserInterface::builder, "gtkMapFrame"); 
     
     drawingArea = gtk_drawing_area_new();
-    //GtkWidget* parent = gtk_widget_get_parent(GTK_WIDGET(gtkMapFrame));
     gtk_container_add(GTK_CONTAINER(gtkMapFrame), p_window);
     gtk_container_add(GTK_CONTAINER(p_window), drawingArea);
     g_signal_connect(G_OBJECT(drawingArea), "draw", G_CALLBACK(cb_draw_callback), NULL);
     surface = gdk_cairo_surface_create_from_pixbuf(pixlBuff, 0, NULL);
+
+    const gchar* filename = "C:\\Users\\Francisco\\source\\repos\\TME\\Tile-Map-Editor---Realmz\\Release\\imgs\\texture-atlas.png";
+    gGraphicsTool->setPixBufSrc(gtk_image_get_pixbuf(GTK_IMAGE(gtk_image_new_from_file(filename))));
+    
+    g_timeout_add(75, push_operations,NULL);
+    g_timeout_add(75, pop_operations, NULL);
 }
+
+gboolean DebugTextureAtlas::push_operations(gpointer data)
+{
+    int countImgs = gResources->getImgPack().getCountImgs(); // count imgs //
+    int random = std::rand() % 100; // random //
+    int maxImgs = 40 + std::rand() % 11; // random [40,50] //
+    
+    // + add operations near 0 imgs // 
+    // + del operations near maxImgs //
+    // 0 <-- del add --> maxImgs //
+    if(random >= countImgs/ maxImgs * 80)
+        operations.push("add");
+    else
+        operations.push("del");
+    return TRUE;
+}
+
+gboolean DebugTextureAtlas::pop_operations(gpointer data)
+{
+    // check if container is not empty //
+    if (!operations.empty()) 
+    {
+        std::string op = operations.top(); //get top value //
+        operations.pop(); // remove top //
+        if (op == "add") // add //
+            addImgs();
+        else if (op == "del") // remove //
+            remImgs();
+    }
+    return TRUE;
+}
+
+
 
 gboolean DebugTextureAtlas::cb_draw_callback(GtkWidget* widget, cairo_t* cr, gpointer data)
 {
+    // draw surface //
     if (surface != nullptr)
     {
-        cairo_set_source_surface(cr, surface, 0, 0);
+        gdk_cairo_set_source_pixbuf(cr,gResources->getImgPack().getTextureAtlas()->getPixelbuf(), 0, 0);
         cairo_paint(cr);
+    }
+    return TRUE;
+}
+
+gboolean DebugTextureAtlas::addImgs()
+{
+    // simulate ims that will be added into this texture atlas //
+    gGraphicsTool->simulate();
+    
+    imgNames.push_back(std::to_string(imgNamesInt)); // add into vector //
+    imgNamesInt++; // ++ //
+
+    // add into ImgPack //
+    gResources->getImgPack().addImgObj(imgNames[imgNames.size()-1], gGraphicsTool->getPixelBufDest(), gGraphicsTool->getImgFormat());
+    
+    // force draw //
+    gtk_widget_queue_draw(GTK_WIDGET(drawingArea));
+    return TRUE;
+}
+
+gboolean DebugTextureAtlas::remImgs()
+{
+    // if container is not empty //
+    if (!imgNames.empty())
+    {   
+        int random = std::rand() % imgNames.size(); // random [0,img size[ //
+        
+        imgNames.erase(imgNames.begin() + random); // erase from this vector //
+        
+        gResources->getImgPack().delImgObj(imgNames[random]); // erase from ImgPack //
+        //gImgPackUI->updateTree(); // update tree view //
+
+        // force draw //
+        gtk_widget_queue_draw(GTK_WIDGET(drawingArea));
+        
     }
     return TRUE;
 }
--- DebugTextureAtlas.h
+++ DebugTextureAtlas.h
@@ -1,12 +1,15 @@
 #pragma once
 #ifdef TME_DEBUG
 #include <gtk/gtk.h>
+#include <vector>
+#include <string>
+#include <stack>
 
 /*!
  Class DebugTextureAtlas
  =======================
 
- This class was created only to test the image insertion, image deletion, memory reallocation and data structure reorganization algorithms.
+ This class was created to test the data structure ImgPack: insertion, deletion and memory reallocation algorithms.
 */
 class DebugTextureAtlas
 {
@@ -21,13 +24,39 @@ public:
 	/**
 	 * rendering function.
 	 */
+	static GtkWidget*  p_window; /**< new window where will be drawn */
+	static GtkWidget* drawingArea; /**< gtk widget drawing area*/
+	static cairo_surface_t* surface;  /**< surface of drawing surface*/
+	static GObject* gtkMapFrame;  /**< container to assist */
+	static int imgNamesInt; /**< used to create unique imgs 1, 2, 3, ... */
+	static std::vector<std::string> imgNames; /**< vector to store imgs created */
+	static std::stack<std::string> operations; /**< stack operations type */
 
-	static GtkWidget*  p_window;
+	/**
+	 * @brief This function adds a new random img (32x32,32x64 or 64x64) into texture atlas pixel buffer.
+	 * It is used to test INSERTION algorithm.
+	 */
+	static gboolean addImgs();
+	/**
+	 * @brief This function removes a random from texture atlas pixel buffer.
+	 * It is used to test REMOVAL algorithm.
+	 */
+	static gboolean remImgs();
 
+	/**
+	 * @brief This function adds a new type of operation randomly on top of stack.
+	 */
+	static gboolean pop_operations(gpointer data);
+
+	/**
+	 * @brief This function removes the last operation add in the stack.
+	 */
+	static gboolean push_operations(gpointer data);
+
+	/**
+	 * @brief This function is used to render the TextureAtlas into the new debug window. 
+	 */	
 	static gboolean cb_draw_callback(GtkWidget* widget, cairo_t* cr, gpointer data);
-	static GtkWidget* drawingArea; /**< drawing area*/
-	static cairo_surface_t* surface;  /**< drawing surface*/
-	static GObject* gtkMapFrame;  /**< drawing container*/
 };
 #endif // TME_DEBUG
 
